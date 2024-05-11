5609cb984c5bef2be2b3f60cf0c009c20ff4137b
CSSkelton <codysskelton@gmail.com>
2024-05-11 10:13:11 -0400

    week 8 assignment

---
+++ src/app/books.service.ts
@@ -0,0 +1,39 @@
+/**
+ * title: books.service.ts
+ * purpose: functional code for book service
+ * author: Cody Skelton
+ * date modified: 05.11.2024
+ * Code sourced from WEB 425 exercise requirements and course repository
+ */
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  isbns: Array<string> = [
+    '0345339681',
+    '0261103571',
+    '9780593099322',
+    '9780261102361',
+    '9780261102378',
+    '9780590302715',
+    '9780316769532',
+    '9780743273565',
+    '9780590405959'
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  getBooks() {
+    let params = new HttpParams();
+
+    params = params.append('bibkeys', `ISBN: ${this.isbns.join(',')}`);
+    params = params.append('format', 'json');
+    params = params.append('jscmd', 'details');
+
+    return this.http.get('https://openlibrary.org/api/books', {params: params})
+  }
+}
